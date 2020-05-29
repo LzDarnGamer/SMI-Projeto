b05eeb87328ed48df382b91b364360cf1479b996
FableArman542 <48763177+FableArman542@users.noreply.github.com>
2020-05-29 19:21:25 +0100

    mais

--- admin/loadUpdate.php
+++ admin/loadUpdate.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
+
+<?php 
+    require_once("../Lib/lib.php");
+    require_once("../Lib/db.php");
+    include( "../ensureAuth.php" );
+
+    $delete = $_POST["delete"];
+    $role = $_POST["role"];
+    $id = $_POST["id"];
+ ?>
 <html>
     <head></head>
     <body>
         <?php echo "Delete: ".$_POST["delete"]; ?>
         <?php echo "Role: ".$_POST["role"]; ?>
         <?php echo "id: ".$_POST["id"]; ?>
+        <?php echo "<br>";
+        echo "real role: ".(getRoleFromUser($id))." New: ".($role)." <br>";?>
     </body>    
 </html>
 
 <?php
 
-require_once("../Lib/lib.php");
-require_once("../Lib/db.php");
-include( "../ensureAuth.php" );
 
-$delete = $_POST["delete"];
-$role = $_POST["role"];
-$id = $_POST["id"];
-echo "<br>";
-echo "real role: ".getRoleFromUser($id)." New: ".$role;
 
 if (strcmp($delete, "yes")) {
-    echo "delete";
+    echo "Delete";
 } else {
-    if ($role == getRoleFromUser($id)) {
-        echo "change role";
-    } else {
+    if (trim($role) === trim(getRoleFromUser($id))) {
         echo "Do nothing";
+    } else {
+        echo "Change Role";
     }
 }
 
