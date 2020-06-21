63129fa9a9cd1dc59eceadad65a90739193166cd
FableArman542 <48763177+FableArman542@users.noreply.github.com>
2020-06-21 23:57:34 +0100

    RSS
    Tou a ter um problema com os caracteres & que no xml dao erro

---
+++ RSS.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once("Lib/lib.php");
+require_once("Lib/db.php");
+
+$users = getAllIds();
+for ($i = 0; $i < sizeof($users); $i ++) {
+    $articlesArray = getArticles($users[$i]);
+}
+
+
+$web_url = "http://" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+
+$str = "<?xml version='1.0' ?>";
+$str .= "<rss version='2.0'>";
+    $str .= "<channel>";
+        $str .= "<title>SMI Project 37</title>";
+        $str .= "<description>Travel Guide</description>";
+        $str .= "<language>en-UK</language>";
+        $str .= "<link>$web_url</link>";
+
+        # Items here
+        for ($i = 0; $i < sizeof($articlesArray); $i ++) {
+            $str .= "<item>";
+                $str .= "<title>" . $articlesArray[$i]['article_title'] . "</title>";
+                $str .= "<description>" . $articlesArray[$i]['article_context'] . "</description>";
+                $str .= "<link>" . $web_url . "/article.php?type=view&id={$articlesArray[$i]['article_id']}" . "</link>";
+            $str .= "</item>";
+        }
+
+
+    $str .= "</channel>";
+$str .= "</rss>";
+
+file_put_contents("rss.xml", $str);
+echo "Done. Redirect back to page.";
+
+?>
